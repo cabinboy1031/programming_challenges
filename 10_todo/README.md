b18b843d591b595927dae072e889c75b489f0c5c
Ian <ianhemmer@live.com>
2020-07-28 16:47:51 -0700

    10_Added basic help message.

---
+++ 10_todo/README.md
@@ -0,0 +1,2 @@
+# A basic TODO list
+## Difficulty: Easy
--- 10_todo/main.cpp
+++ 10_todo/main.cpp
@@ -16,15 +16,27 @@ int main(int argc, char** argv){
   do{
 		getline(std::cin, command);
     switch (command[0]){
-    case 'a':
-      add_item(todoList);
-      break;
-    case 'd':
-      delete_item(todoList);
-      break;
-    case 't':
-      print_days(todoList,0,0);
-      break;
+    	case 'a':
+				add_item(todoList);
+				break;
+			case 'd':
+				delete_item(todoList);
+				break;
+			case 't':
+				print_days(todoList,0,0);
+				break;
+			case 's':
+				// save_list();
+			case 'l':
+				// load_list();
+			case 'h':
+				std::cout << "Help:" << std::endl
+				  << "a: Add item to a list" << std::endl
+					<< "d: Delete an item from the list" << std::endl
+					<< "t: Print the current todo list" << std::endl
+					<< "h: display this screen" << std::endl
+					<< "q: quit the program" << std::endl << std::endl
+					<< "No matter what you write down, the first character of the given input is the only one that is processed." << std::endl;
     }
     std::cout << "Insert a command (H for help)" << std::endl << "> ";
   } while(command != "q");
