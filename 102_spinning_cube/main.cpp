45b61850a172f5478839cd46df5f4bbd705d8a75
Ian <ianhemmer@live.com>
2020-08-13 10:59:23 -0700

    102_New GLSL boilerplate

--- 102_spinning_cube/main.cpp
+++ 102_spinning_cube/main.cpp
@@ -4,8 +4,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-
-
 int main(){
 	if (!glfwInit()) {
     fprintf(stderr, "ERROR: could not start GLFW3\n");
@@ -34,8 +32,64 @@ int main(){
   glEnable(GL_DEPTH_TEST); // enable depth-testing
   glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
 
-/* OTHER STUFF GOES HERE NEXT */
+	// Point object
+	float points[] = {
+   0.0f,  0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f,
+  -0.5f, -0.5f,  0.0f
+	};
+
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 9* sizeof(float),points,GL_STATIC_DRAW);
   
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	const char* vertex_shader =
+	"#version 400\n"
+	"in vec3 vp;"
+	"void main() {"
+	"  gl_Position = vec4(vp, 1.0);"
+	"}";
+
+	const char* fragment_shader =
+	"#version 400\n"
+	"out vec4 frag_colour;"
+	"void main() {"
+	"  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+	"}";
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs,1,&vertex_shader,NULL);
+	glCompileShader(vs);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs,1,&fragment_shader,NULL);
+	glCompileShader(fs);
+
+	GLuint shader_programme = glCreateProgram();
+	glAttachShader(shader_programme, fs);
+	glAttachShader(shader_programme, vs);
+	glLinkProgram(shader_programme);
+
+	while(!glfwWindowShouldClose(window)){
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(shader_programme);
+		glBindVertexArray(vao);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwPollEvents();
+
+		glfwSwapBuffers(window);
+	}
+
+
   // close GL context and any other GLFW resources
   glfwTerminate();
 	return 0;
